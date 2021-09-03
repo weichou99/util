7fc630af1e449cf1e06b33277d94bd7e4ed1f62e
carter <weichou99@gmail.com>
2021-09-03 14:42:16 +0800

    新增有壓縮。

--- watch/src/watch/DownloadServlet.java
+++ watch/src/watch/DownloadServlet.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,28 +16,60 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/download")
 public class DownloadServlet extends HttpServlet {
-	private final int ARBITARY_SIZE = 1048;
+  private final int ARBITARY_SIZE = 1048;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String filename = req.getParameter("filename");
+  // ¦³À£ÁY¡Agz¡C
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String filename = req.getParameter("filename");
 
-		String catalinaBase = System.getProperty("catalina.base");
-		File catalinaBaseFile = new File(catalinaBase);
-		File logsFile = new File(catalinaBaseFile, "logs");
-		File file = new File(logsFile, filename);
+    String catalinaBase = System.getProperty("catalina.base");
+    File catalinaBaseFile = new File(catalinaBase);
+    File logsFile = new File(catalinaBaseFile, "logs");
+    File file = new File(logsFile, filename);
 
-		resp.setContentType("text/plain");
-		resp.setHeader("Content-disposition", "attachment; filename=" + filename);
+    long length = file.length();
 
-		try (InputStream in = new FileInputStream(file); OutputStream out = resp.getOutputStream()) {
+    resp.setContentType("text/plain");
+    resp.addHeader("Content-Encoding", "gzip");
 
-			byte[] buffer = new byte[ARBITARY_SIZE];
+    resp.setHeader("Content-disposition", "attachment; filename=" + filename);
 
-			int numBytesRead;
-			while ((numBytesRead = in.read(buffer)) > 0) {
-				out.write(buffer, 0, numBytesRead);
-			}
-		}
-	}
+    try (InputStream in = new FileInputStream(file); OutputStream out = resp.getOutputStream()) {
+      GZIPOutputStream gzipo = new GZIPOutputStream(out);
+
+      byte[] buffer = new byte[ARBITARY_SIZE];
+
+      int numBytesRead;
+      while ((numBytesRead = in.read(buffer)) > 0) {
+        System.out.println(numBytesRead);
+        gzipo.write(buffer, 0, numBytesRead);
+      }
+      gzipo.finish();
+    }
+  }
+
+  // ¨S¦³À£ÁY
+  protected void doGetOld(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String filename = req.getParameter("filename");
+
+    String catalinaBase = System.getProperty("catalina.base");
+    File catalinaBaseFile = new File(catalinaBase);
+    File logsFile = new File(catalinaBaseFile, "logs");
+    File file = new File(logsFile, filename);
+
+    resp.setContentType("text/plain");
+    resp.setHeader("Content-disposition", "attachment; filename=" + filename);
+
+    try (InputStream in = new FileInputStream(file); OutputStream out = resp.getOutputStream()) {
+
+      byte[] buffer = new byte[ARBITARY_SIZE];
+
+      int numBytesRead;
+      while ((numBytesRead = in.read(buffer)) > 0) {
+        out.write(buffer, 0, numBytesRead);
+      }
+    }
+  }
+
 }
